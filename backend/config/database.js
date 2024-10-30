83081e8ca434f6ea7fc113c12fbf775f681f7ad9
TarunGanesh <tarun.21bce8974@vitapstudent.ac.in>
2024-10-30 21:20:50 +0530

    database config

---
+++ backend/config/database.js
@@ -0,0 +1,9 @@
+// server/config/database.js
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+});
+
+module.exports = sequelize;
